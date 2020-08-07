d13cbe0514756bf4d4658bb632a8abb9288306d1
scottdavisxx <scottdavisxx@gmail.com>
2020-08-06 21:57:02 -0700

    hashed password using bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,36 +2,34 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
-
-
-router.get('/',(req,res)=> {
-    res.send("hello")
-})
+const bcrypt = require('bcrypt')
 
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
     if(!name || !email || !password){
-        return res.status(422).json({err: "please enter all required fields"})
+        return res.status(422).json({error: "please enter all required fields"})
     }
     User.findOne({email:email})
     .then((savedUser)=>{
         if(savedUser){
-            return res.status(422).json({err: "user already exists with that email"})
+            return res.status(422).json({error: "user already exists with that email"})
         }
-        const user = new User({
-            name,
-            email,
-            password
+        bcrypt.hash(password, 12)
+        .then(hashedpassword=>{
+            const user = new User({
+                name,
+                email,
+                password:hashedpassword
+            }) 
+        
+            user.save()
+            .then(user=>{
+                res.json({message:"saved sucessfully"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         })
-
-        user.save()
-        .then(user=>{
-            res.json({message:"saved sucessfully"})
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-
     })
     .catch(err=>{
         console.log(err) 
