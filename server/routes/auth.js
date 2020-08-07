1d8281ac2d452f9129f8d78f994944ac5aeb7620
scottdavisxx <scottdavisxx@gmail.com>
2020-08-06 21:18:31 -0700

    before downloading postman

---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/',(req,res)=> {
+    res.send("hello")
+})
+
+router.post('/signup',(req,res)=>{
+    console.log(req.body.name)
+})
+
+module.exports = router
