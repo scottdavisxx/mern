1d73441923a0d0ce638bc0bb4320770489f8f402
scottdavisxx <scottdavisxx@gmail.com>
2021-03-10 21:09:55 -0800

    working before data.map bug

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -9,31 +9,30 @@ const Home = ()=>{
             }
         }).then(res=>res.json())
         .then(result=>{
-            setData(result.posts)
-        })
+            setData(result.posts)        })
     },[])
 
-    
+
     return(
         <div className='home'>
             {
-                data.map(item=>{
-                    return(
-                        <div className='card home-card'>
-                        <h5>Scott</h5>
-                            <div className='card-image'>
-                                <img src='https://i.imgur.com/rP6TgQd.jpeg'/>
-                            </div>
-                            <div className='card-content'>
-                                <i className="material-icons" style={{color: 'red'}}>favorite</i>
-                                <h6>title</h6>
-                                <p>This is an amazing post</p>
-                                <input type='text' placeholder='comment'/>
-                            </div>
-                        </div>
-                    )
-                })
+                <div className='card home-card'>
+                <h5>Scott</h5>
+                    <div className='card-image'>
+                        <img src='https://i.imgur.com/rP6TgQd.jpeg'/>
+                    </div>
+                    <div className='card-content'>
+                        <i className="material-icons" style={{color: 'red'}}>favorite</i>
+                        <h6>title</h6>
+                        <p>This is an amazing post</p>
+                        <input type='text' placeholder='comment'/>
+                    </div>
+                </div>
             }
+                 
+
+        )
+            
 
         </div>
     )
