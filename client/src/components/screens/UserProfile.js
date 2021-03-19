25b4a89e7f632dea3bc1a57d76e05244ede3f4c1
scottdavisxx <scottdavisxx@gmail.com>
2021-03-18 20:01:42 -0700

    code up to 1238, not working checking source

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -3,7 +3,7 @@ import {UserContext} from '../../App'
 import {useParams} from 'react-router-dom'
 
 const UserProfile = ()=>{
-    const [mypics,setPics] = useState([])
+    const [userProfile,setProfile] = useState(null)
     const {state,dispatch} = useContext(UserContext)
     const {userid} = useParams()
     console.log(userid)
@@ -15,44 +15,50 @@ const UserProfile = ()=>{
        }).then(res=>res.json())
        .then(result=>{
         console.log(result)
+        setProfile(result)
        })
     },[])
     return(
+      <>
+      {userProfile ? 
         <div style={{maxWidth: '1400px', margin: '0 auto',}}>
-            <div style={{display: 'flex', 
-                         justifyContent: 'space-around',
-                         margin: '18px auto',
-                         borderBottom: '1px solid gray',
-                         maxWidth: '550px',}}>
-                <div>
-                    <img style={{width: '160px', height: '160px', borderRadius: '80px'}}
-                    src='https://i.imgur.com/z2U2iYx.jpg'></img>
+        <div style={{display: 'flex', 
+                     justifyContent: 'space-around',
+                     margin: '18px auto',
+                     borderBottom: '1px solid gray',
+                     maxWidth: '550px',}}>
+            <div>
+                <img style={{width: '160px', height: '160px', borderRadius: '80px'}}
+                src='https://i.imgur.com/z2U2iYx.jpg'></img>
+            </div>
+            <div>
+                <h4>{userProfile.user.name}</h4>
+                <h5>{userProfile.user.email}</h5>
+                <div style={{display: 'flex',
+                             justifyContent: 'space-between',
+                             width: '108%',}}>
+                    <h6>{userProfile.posts.length}</h6>
+                    <h6>50 followers</h6>
+                    <h6>60 following</h6>
                 </div>
-                <div>
-                    <h4>{state?state.name:"loading..."}</h4>
-                    <div style={{display: 'flex',
-                                 justifyContent: 'space-between',
-                                 width: '108%',}}>
-                        <h6>40 posts</h6>
-                        <h6>50 followers</h6>
-                        <h6>60 following</h6>
-                    </div>
-                </div>
-
             </div>
 
-            <div className='gallery'>
-                {
-                    mypics.map(item=>{
-                        return(
-                            <img key={item._id} className='item' src={item.photo} alt={item.title} />
-                        )
-                    })
-                }
-
-
-            </div>
         </div>
+        <div className='gallery'>
+            {
+                userProfile.posts.map(item=>{
+                    return(
+                        <img key={item._id} className='item' src={item.photo} alt={item.title} />
+                    )
+                })
+            }
+
+
+        </div>
+    </div>
+      : <h2>loading...</h2>}
+        
+      </>
     )
 }
 
