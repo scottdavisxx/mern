8983d62542039512907206b73db7b60f776dc5b3
Scott Davis <sd@scotts-mbp.lan>
2021-03-30 20:34:56 -0700

    add upload profile pic button to profile screen

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -16,29 +16,33 @@ const Profile = ()=>{
     },[])
     return(
         <div style={{maxWidth: '1400px', margin: '0 auto',}}>
-            <div style={{display: 'flex', 
-                         justifyContent: 'space-around',
-                         margin: '18px auto',
-                         borderBottom: '1px solid gray',
-                         maxWidth: '550px',}}>
-                <div>
-                    <img style={{width: '160px', height: '160px', borderRadius: '80px'}}
-                    src={state?state.pic:"https://res.cloudinary.com/scottdavisxx/image/upload/v1617075577/ProfileDefault_zmett8.png"}></img>
-                </div>
-                <div>
-                    <h4>{state?state.name:"loading..."}</h4>
-                    <h5>{state?state.email:"loading..."}</h5>
-                    <div style={{display: 'flex',
-                                 justifyContent: 'space-between',
-                                 width: '108%',}}>
-                        <h6>{mypics.length} posts</h6>
-                        <h6>{state?state.followers.length:".."} followers</h6>
-                        <h6>{state?state.following.length:".."} following</h6>
+            <div style={{ margin: '18px auto',
+                    borderBottom: '1px solid gray',
+                    maxWidth: '550px',}}>
+                <div style={{display: 'flex', 
+                            justifyContent: 'space-around',
+                            margin: '18px auto'}}>
+                    <div>
+                        <img style={{width: '160px', height: '160px', borderRadius: '80px'}}
+                        src={state?state.pic:"https://res.cloudinary.com/scottdavisxx/image/upload/v1617075577/ProfileDefault_zmett8.png"}></img>
                     </div>
+                    <div>
+                        <h4>{state?state.name:"loading..."}</h4>
+                        <h5>{state?state.email:"loading..."}</h5>
+                        <div style={{display: 'flex',
+                                    justifyContent: 'space-between',
+                                    width: '108%',}}>
+                            <h6>{mypics.length} posts</h6>
+                            <h6>{state?state.followers.length:".."} followers</h6>
+                            <h6>{state?state.following.length:".."} following</h6>
+                        </div>
+                    </div>
+
                 </div>
-
-            </div>
-
+                    <button className="btn waves-effect waves-light blue darken">
+                        Update Profile Pic
+                    </button>
+                </div>
             <div className='gallery'>
                 {
                     mypics.map(item=>{
