184d77343b00db26b30f207728fec458f913f7c7
Scott Davis <sd@scotts-mbp.lan>
2021-03-31 20:09:19 -0700

    update route for updatepic in user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -65,4 +65,14 @@ router.put('/unfollow',requireLogin,(req,res)=>{
   )
 })
 
+router.put('/updatepic',(req,res)=>{
+  User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}},{new:true},
+    (err,result)=>{
+      if(err){
+        return res.status(422).json({error:"Pic cannot post"})
+      }
+      res.json(result)
+  })
+})
+
 module.exports = router
